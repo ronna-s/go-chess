e0b110da469343dc6abb2533217393157f706950
Ronna Steinberg <ronna.steinberg@daliaresearch.com>
2018-02-12 11:20:58 +0100

    added clasess to images

--- board.go
+++ board.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	. "github.com/notnil/chess"
-	"strconv"
 )
 
 type piece struct {
 	ImgPath string
-	PieceId string
+	Type string
 }
 
 type square struct {
@@ -15,22 +14,23 @@ type square struct {
 	Color bool
 	Row int
 	Col string
+	Id int
 }
 
 var pieces = map[Piece]piece{
-	BlackQueen:  {ImgPath: "/images/queen-3d-pink.png", PieceId: "black_queen"},
-	BlackKing:   {ImgPath: "/images/king-3d-pink.png", PieceId: "back_king"},
-	BlackBishop: {ImgPath: "/images/bishop-3d-pink-big-hat-2.png", PieceId: "black_bishop"},
-	BlackKnight: {ImgPath: "/images/knight-3d-pink.png", PieceId: "black-knight"},
-	BlackRook:   {ImgPath: "/images/rook-3d-pink.png", PieceId: "black-rook"},
-	BlackPawn:   {ImgPath: "/images/pawn-3d-pink.png", PieceId: "black-pawn"},
-	WhiteQueen:  {ImgPath: "/images/queen-3d.png", PieceId: "white_queen"},
-	WhiteKing:   {ImgPath: "/images/king-3d.png", PieceId: "white_king"},
-	WhiteBishop: {ImgPath: "/images/bishop-3d-big-hat-2.png", PieceId: "white_bishop"},
-	WhiteKnight: {ImgPath: "/images/knight-3d.png", PieceId: "white_knight"},
-	WhiteRook:   {ImgPath: "/images/rook-3d.png", PieceId: "white_rook"},
-	WhitePawn:   {ImgPath: "/images/pawn-3d.png", PieceId: "white_pawn"},
-	NoPiece:     {ImgPath: "/images/transparent.png", PieceId: "no_piece"},
+	BlackQueen:  {ImgPath: "/images/queen-3d-pink.png", Type: "black_queen"},
+	BlackKing:   {ImgPath: "/images/king-3d-pink.png", Type: "black_king"},
+	BlackBishop: {ImgPath: "/images/bishop-3d-pink-big-hat-2.png", Type: "black_bishop"},
+	BlackKnight: {ImgPath: "/images/knight-3d-pink.png", Type: "black_knight"},
+	BlackRook:   {ImgPath: "/images/rook-3d-pink.png", Type: "black_rook"},
+	BlackPawn:   {ImgPath: "/images/pawn-3d-pink.png", Type: "black_pawn"},
+	WhiteQueen:  {ImgPath: "/images/queen-3d.png", Type: "white_queen"},
+	WhiteKing:   {ImgPath: "/images/king-3d.png", Type: "white_king"},
+	WhiteBishop: {ImgPath: "/images/bishop-3d-big-hat-2.png", Type: "white_bishop"},
+	WhiteKnight: {ImgPath: "/images/knight-3d.png", Type: "white_knight"},
+	WhiteRook:   {ImgPath: "/images/rook-3d.png", Type: "white_rook"},
+	WhitePawn:   {ImgPath: "/images/pawn-3d.png", Type: "white_pawn"},
+	NoPiece:     {ImgPath: "/images/transparent.png", Type: "no_piece"},
 }
 
 func draw(b *Board) [][]square {
@@ -38,18 +38,16 @@ func draw(b *Board) [][]square {
 	isWhite := false
 	rows := []string{"a","b","c","d","e","f","g","h"}
 	for r := 7; r >= 0; r-- {
-		board[r] = make([]square, 8)
+		board[7-r] = make([]square, 8)
 		for f := 0; f < 8; f++ {
 			s := r*8 + f
 			p := pieces[b.Piece(Square(s))]
-			board[r][f] = square{
+			board[7-r][f] = square{
 				Row: r+1,
 				Col: rows[f],
-				piece: piece{
-					PieceId: p.PieceId + strconv.Itoa(s),
-					ImgPath: p.ImgPath,
-				},
+				piece: p,
 				Color: isWhite,
+				Id: r * 8 + f,
 			}
 			isWhite = !isWhite
 		}
