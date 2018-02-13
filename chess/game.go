2564942872cb464f5ad52697c6344a9844d4169d
Ronna Steinberg <ronna.steinberg@daliaresearch.com>
2018-02-13 17:22:28 +0100

    code better

---
+++ chess/game.go
@@ -0,0 +1,92 @@
+package chess
+
+import (
+	"errors"
+
+	"fmt"
+
+	"github.com/notnil/chess"
+)
+
+type (
+	Game struct {
+		ptr *chess.Game
+	}
+)
+
+func NewGame() *Game {
+	return &Game{chess.NewGame()}
+}
+
+func (g *Game) Move(m Move) error {
+	validMoves := g.ptr.ValidMoves()
+	for i := range validMoves {
+		move := validMoves[i]
+		if move.S1() == chess.Square(m.from) &&
+			move.S2() == chess.Square(m.to) {
+			g.ptr.Move(move)
+			return nil
+		}
+	}
+	return errors.New("move is invalid")
+}
+
+func (g *Game) Promote(p Promotion) error {
+	validMoves := g.ptr.ValidMoves()
+	for i := range validMoves {
+		move := validMoves[i]
+		if move.S1() == chess.Square(p.from) &&
+			move.S2() == chess.Square(p.to) &&
+			move.Promo().String() == p.newPiece {
+			fmt.Println(move)
+			g.ptr.Move(move)
+			return nil
+		}
+	}
+	return errors.New("promotion is invalid")
+}
+
+func (g *Game) Draw() [][]square {
+	board := make([][]square, 8)
+	isWhite := false
+	for r := 7; r >= 0; r-- {
+		board[7-r] = make([]square, 8)
+		for f := 0; f < 8; f++ {
+			pos := r*8 + f
+			p := g.getPiece(pos)
+			board[7-r][f] = square{
+				Piece: p,
+				Color: Color(isWhite),
+				Pos:   pos,
+			}
+			isWhite = !isWhite
+		}
+		isWhite = !isWhite
+	}
+	return board
+}
+
+func (g *Game) getPiece(pos int) piece {
+	b := g.ptr.Position().Board()
+	return pieces[b.Piece(chess.Square(pos))]
+}
+func (g *Game) Piece(pos int) {
+	g.ptr.Position().Board().Piece(chess.Square(pos))
+}
+
+func (g *Game) Debug() string {
+	return g.ptr.Position().Board().Draw()
+}
+
+func (g *Game) ValidPromotions(m Move) (pieces []piece) {
+	validMoves := g.ptr.ValidMoves()
+	color := g.getPiece(m.from).Color
+	for i := range validMoves {
+		move := validMoves[i]
+		if move.S1() == chess.Square(m.from) &&
+			move.S2() == chess.Square(m.to) {
+			pieces = append(pieces, piece{Id: move.Promo().String(), Color: color})
+		}
+	}
+	return
+}
--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"github.com/notnil/chess"
 	"bytes"
 	"html/template"
-	"strconv"
+	"log"
+	"net/http"
 	"strings"
+
+	"fmt"
+
+	"github.com/wwgberlin/go-event-sourcing-exercise/chess"
+)
+
+const (
+	EventMove int = iota
+	EventPromotion
+	EventRollback
 )
 
 type Event struct {
-	id   int
-	move string
+	id        int
+	eventData string
+	eventType int
 }
 
 func nextID(events []Event) int {
@@ -29,15 +37,10 @@ var events []Event
 func main() {
 
 	http.HandleFunc("/game/", func(writer http.ResponseWriter, request *http.Request) {
-		game := chess.NewGame()
-
-		for _, event := range events {
-			game.Move(queryToMove(game, event.move))
-		}
-
+		game := buildGame(events)
 		var tpl bytes.Buffer
 		t := template.Must(template.ParseFiles("templates/board.tmpl", "templates/game.tmpl"))
-		if err := t.ExecuteTemplate(&tpl, "base", draw(game.Position().Board())); err != nil {
+		if err := t.ExecuteTemplate(&tpl, "base", game.Draw()); err != nil {
 			panic(err)
 		}
 		writer.Write(tpl.Bytes())
@@ -47,7 +50,7 @@ func main() {
 
 	http.Handle("/images/", http.StripPrefix("/", http.FileServer(http.Dir("./public"))))
 
-	http.HandleFunc("/board", boardHandler)
+	http.HandleFunc("/debug", debugHandler(game))
 	http.HandleFunc("/move", moveHandler(game))
 	http.HandleFunc("/promote", promoteHandler(game))
 	http.HandleFunc("/valid_promotions", validPromotions(game))
@@ -55,42 +58,47 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func queryToMove(game *chess.Game, query string) *chess.Move {
-	moves := strings.Split(query, "-")
-	fromSquare, _ := strconv.ParseInt(moves[0], 10, 8)
-	toSquare, _ := strconv.ParseInt(moves[1], 10, 8)
-	var promo string
-	if len(moves) > 2 {
-		promo = moves[2]
-	}
-	for _, move := range game.ValidMoves() {
-		if move.S1() == chess.Square(fromSquare) &&
-			move.S2() == chess.Square(toSquare) &&
-			move.Promo().String() == promo {
-			return move
+func buildGame(events []Event) *chess.Game {
+	game := chess.NewGame()
+
+	for _, event := range events {
+		switch event.eventType {
+		case EventMove:
+			game.Move(parseMove(event.eventData))
+		case EventPromotion:
+			game.Promote(parsePromotion(event.eventData))
+		case EventRollback:
+			//todo
 		}
 	}
-	return nil
+	return game
 }
-func queryToPromotion(game *chess.Game, query string) *chess.Move {
-	moves := strings.Split(query, "-")
-	fromSquare, _ := strconv.ParseInt(moves[0], 10, 8)
-	toSquare, _ := strconv.ParseInt(moves[1], 10, 8)
-	for _, move := range game.ValidMoves() {
-		if move.S1() == chess.Square(fromSquare) &&
-			move.S2() == chess.Square(toSquare) &&
-			move.Promo().String() == moves[2] {
-			return move
-		}
+
+func parseMove(query string) chess.Move {
+	var from, to int
+	if _, err := fmt.Sscanf(query, "%d-%d", &from, &to); err != nil {
+		log.Println(err)
 	}
-	return nil
+	return chess.NewMove(from, to)
+}
+func parsePromotion(query string) chess.Promotion {
+	var (
+		from, to int
+		newPiece string
+	)
+
+	if _, err := fmt.Sscanf(query, "%d-%d-%s", &from, &to, &newPiece); err != nil {
+		log.Println(err)
+	}
+
+	return chess.NewPromotion(from, to, newPiece)
 }
 
 // move handler performs data validation and writes it to the event store if everything is correct
 func moveHandler(game *chess.Game) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		query := r.URL.RawQuery
-		move := queryToMove(game, query)
+		move := parseMove(query)
 		if err := game.Move(move); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			if _, err := w.Write([]byte("Invalid move")); err != nil {
@@ -99,14 +107,15 @@ func moveHandler(game *chess.Game) func(w http.ResponseWriter, r *http.Request) 
 			log.Println(err)
 		} else {
 			events = append(events, Event{
-				id:   nextID(events),
-				move: query,
+				id:        nextID(events),
+				eventType: EventMove,
+				eventData: query,
 			})
 
 			w.WriteHeader(http.StatusOK)
 			var tpl bytes.Buffer
 			t := template.Must(template.ParseFiles("templates/board.tmpl"))
-			if err := t.ExecuteTemplate(&tpl, "board", draw(game.Position().Board())); err != nil {
+			if err := t.ExecuteTemplate(&tpl, "board", game.Draw()); err != nil {
 				panic(err)
 			}
 			w.Write(tpl.Bytes())
@@ -119,8 +128,8 @@ func moveHandler(game *chess.Game) func(w http.ResponseWriter, r *http.Request) 
 func promoteHandler(game *chess.Game) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		query := r.URL.RawQuery
-		move := queryToPromotion(game, query)
-		if err := game.Move(move); err != nil {
+		promotion := parsePromotion(query)
+		if err := game.Promote(promotion); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			if _, err := w.Write([]byte("Invalid move")); err != nil {
 				log.Printf("can't write the response: %v", err)
@@ -128,14 +137,15 @@ func promoteHandler(game *chess.Game) func(w http.ResponseWriter, r *http.Reques
 			log.Println(err)
 		} else {
 			events = append(events, Event{
-				id:   nextID(events),
-				move: query,
+				id:        nextID(events),
+				eventData: query,
+				eventType: EventPromotion,
 			})
 
 			w.WriteHeader(http.StatusOK)
 			var tpl bytes.Buffer
 			t := template.Must(template.ParseFiles("templates/board.tmpl"))
-			if err := t.ExecuteTemplate(&tpl, "board", draw(game.Position().Board())); err != nil {
+			if err := t.ExecuteTemplate(&tpl, "board", game.Draw()); err != nil {
 				panic(err)
 			}
 			w.Write(tpl.Bytes())
@@ -146,39 +156,25 @@ func promoteHandler(game *chess.Game) func(w http.ResponseWriter, r *http.Reques
 
 // boardHandler writes string representation of current board state to http response
 // it doesn't have any information about current game, only a list of moves, from which it builds the state
-func boardHandler(w http.ResponseWriter, r *http.Request) {
-	game := chess.NewGame()
-
-	for _, event := range events {
-		move := queryToMove(game, event.move)
-		game.Move(move)
-	}
-
-	if _, err := w.Write([]byte(game.Position().Board().Draw())); err != nil {
-		log.Printf("can't write the response: %v", err)
+func debugHandler(game *chess.Game) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		game := buildGame(events)
+		if _, err := w.Write([]byte(game.Debug())); err != nil {
+			log.Printf("can't write the response: %v", err)
+		}
 	}
 }
 
 func validPromotions(game *chess.Game) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		query := r.URL.RawQuery
-		moves := strings.Split(query, "-")
-		fromSquare, _ := strconv.ParseInt(moves[0], 10, 8)
-		toSquare, _ := strconv.ParseInt(moves[1], 10, 8)
-		color := ""
-		if chess.White == game.Position().Board().Piece(chess.Square(fromSquare)).Color() {
-			color = "white"
-		} else {
-			color = "black"
+		move := parseMove(query)
+		promotions := game.ValidPromotions(move)
+		strs := make([]string, len(promotions))
+		for i := range promotions {
+			strs[i] = promotions[i].ImagePath()
 		}
-		var promotions []string
-		for i := range game.ValidMoves() {
-			move := game.ValidMoves()[i]
-			if move.S1() == chess.Square(fromSquare) && move.S2() == chess.Square(toSquare) {
-				promotions = append(promotions, "/images/"+move.Promo().String()+"-"+color+".png")
-			}
-		}
-		w.Write([]byte(strings.Join(promotions, ",")))
+		w.Write([]byte(strings.Join(strs, ",")))
 		return
 
 	}
